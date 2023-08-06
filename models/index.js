fd3c1d6a93e5289d94785342eb79e843d3d844e8
Faith Fechser <faithfechser@gmail.com>
2023-08-06 03:24:17 -0600

    code belongsTo and category code blocks

--- models/index.js
+++ models/index.js
@@ -10,18 +10,21 @@ Product.belongsTo(Category, {
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  }
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
+  through: {
+    model: ProductTag,
+    unique: false
+  }
 });
 
 module.exports = {
